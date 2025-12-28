b5c3dd9af9a9d9ba8582177ff8e530fc1b449c58
10SEBASG01 <jandryelpro.ec@gmail.com>
2025-12-27 22:33:18 -0500

    Doceavo Commit Del Proyecto

--- build.sh
+++ build.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
 pip install -r requirements.txt
-
 python manage.py collectstatic --no-input
 python manage.py migrate
 
+# --- COMANDO DE LIMPIEZA TOTAL ---
+# Borra el historial de acciones recientes (lo que se ve feo)
+# Y borra todos los datos de tus tablas de Perfil para empezar de cero
+echo "from django.contrib.admin.models import LogEntry; \
+from Perfil.models import DatosPersonales; \
+LogEntry.objects.all().delete(); \
+DatosPersonales.objects.all().delete();" \
+| python manage.py shell
+
+# Crear/Asegurar superusuario
 echo "from django.contrib.auth import get_user_model; \
 User = get_user_model(); \
 User.objects.filter(username='Jandry').exists() or \
