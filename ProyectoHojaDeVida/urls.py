a76a2181d0007b2beeaf5baf8b4855b41df2c662
10SEBASG01 <jandryelpro.ec@gmail.com>
2025-12-30 17:48:40 -0500

    4

--- ProyectoHojaDeVida/urls.py
+++ ProyectoHojaDeVida/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from Perfil import views # Importamos tus vistas
+from django.contrib.auth import views as auth_views
+from Perfil import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('', views.home, name='home'),
     path('experiencia/', views.experiencia, name='experiencia'),
     path('productos-academicos/', views.productos_academicos, name='productos_academicos'),
@@ -13,8 +15,11 @@ urlpatterns = [
     path('cursos/', views.cursos, name='cursos'),
     path('reconocimientos/', views.reconocimientos, name='reconocimientos'),
     path('garage/', views.garage, name='garage'),
+
+    # 🔐 Auth
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
 
-# ESTO ES LO QUE HACE QUE EL NAVEGADOR PUEDA ABRIR TUS DIPLOMAS
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
